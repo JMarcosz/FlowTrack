067acbc8104ea1bd06f18b131de54931479f6a6c
the_sebas0 <sebastianmarte25@gmail.com>
2026-05-15 15:09:16 -0400

    Sprint 3-4 "Update de Enums"

--- app/src/main/java/com/example/flowtrack/domain/model/Enums.kt
+++ app/src/main/java/com/example/flowtrack/domain/model/Enums.kt
@@ -5,9 +5,9 @@ enum class Moneda { DOP, USD }
 enum class TipoCuenta { CORRIENTE, AHORRO, CREDITO, INVERSION }
 enum class TipoDocumento { CUENTA_CORRIENTE, CUENTA_AHORRO, TARJETA_CREDITO, TARJETA_DEBITO, PRESTAMO, INVERSION }
 enum class EstadoCarga { EXITOSO, PARCIAL, FALLIDO }
-enum class TipoMatch { IGUAL, CONTIENE, EMPIEZA_CON, REGEX }
+enum class TipoMatch { EXACTO, CONTIENE, EMPIEZA_CON, REGEX }
 enum class OrigenTasa { AUTO_EXTRAIDA, MANUAL }
-enum class EstadoTarjeta { ACTIVO }
+enum class EstadoTarjeta { ACTIVO, INACTIVO }
 
 enum class TipoMovimientoTarjeta {
     COMPRA, PAGO, INTERES, COMISION, CASHBACK, AJUSTE, AVANCE_EFECTIVO
